5dc51686afe566c56883fc1d8db27a096f3e4d6c
Rohit Deshmukh <rohit.deshmukh119@gmail.com>
2016-11-04 03:54:18 +0530

    Form Styling 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = { :host => 'https://immense-ridge-90621.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'https://immense-ridge-90621.herokuapp.com' }
   
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
